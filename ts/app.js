6fcf1f7a4cb0881904c5f292590839e6cacdb467
eugenioclrc <eugenioclrc@gmail.com>
2016-03-07 15:38:15 -0300

    del builded files

--- ts/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require("./load");
-$(function () {
-    console.log('yehaa');
-    $('body').html('<h1>This content has been load with jQuery</h1>');
-});
-//# sourceMappingURL=app.js.map
